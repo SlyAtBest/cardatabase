759b4c7ef2177843e2fa032bbff25bab04a1eb8b
Mat Durrant <matd@Mats-MacBook-Air.local>
2021-08-07 15:45:23 +0100

    Add first rest controller to return cars

--- src/main/java/com/packt/cardatabase/domain/Owner.java
+++ src/main/java/com/packt/cardatabase/domain/Owner.java
@@ -1,5 +1,7 @@
 package com.packt.cardatabase.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -15,6 +17,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Owner {
 
   @Id
@@ -23,6 +26,7 @@ public class Owner {
   private String firstName, lastName;
 
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+  @JsonIgnore
   private List<Car> cars;
 
   public Owner(String firstName, String lastName) {
